6402e9376599f40190b9149272a2e2b09c4ec303
Leonardo Nascimento <leonascimento.dev@gmail.com>
2022-08-08 22:49:38 -0300

    Updates

---
+++ precache-manifest.5189f0d0a72809f87b3396951b77b85f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9bcacc6215ab3a0e31",
+    "url": "/calculadora-app/static/css/main.7b5dc5ec.chunk.css"
+  },
+  {
+    "revision": "2c9bcacc6215ab3a0e31",
+    "url": "/calculadora-app/static/js/main.2c9bcacc.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/calculadora-app/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "3c4c0a063786a8bba1c6",
+    "url": "/calculadora-app/static/js/2.3c4c0a06.chunk.js"
+  },
+  {
+    "revision": "500e1de2bbaee06b82f7",
+    "url": "/calculadora-app/static/js/runtime~main.500e1de2.js"
+  },
+  {
+    "revision": "d1a7b45f28bf337cab8adf3992f669a0",
+    "url": "/calculadora-app/static/media/RobotoMono-Thin.d1a7b45f.ttf"
+  },
+  {
+    "revision": "03eea07683d0a8fa30c1ccd551628e62",
+    "url": "/calculadora-app/index.html"
+  }
+];
